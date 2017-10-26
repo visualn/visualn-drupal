97695c3f0c8624e587e9ec8ed5e03c0b7e29b00b
kpv <kpv@758986.no-reply.drupal.org>
2017-10-26 12:52:51 +0000

    Issue #2918990: Create a Slick generic drawer

---
+++ modules/visualn_styles/src/Plugin/VisualN/Drawer/SlickDrawer.php
@@ -0,0 +1,95 @@
+<?php
+
+/**
+ * @file
+ * Conatins SlickDrawer based for Slick.js library
+ */
+
+namespace Drupal\visualn_styles\Plugin\VisualN\Drawer;
+
+use Drupal\visualn\Plugin\VisualNDrawerBase;
+use Drupal\Core\Form\FormStateInterface;
+
+/**
+ * Provides a 'SlickDrawer' VisualN drawer.
+ *
+ * @VisualNDrawer(
+ *  id = "visualn_slick",
+ *  label = @Translation("Slick"),
+ * )
+ */
+class SlickDrawer extends VisualNDrawerBase {
+
+  /**
+   * @inheritdoc
+   */
+  public function prepareBuild(array &$build, $vuid, array $options = []) {
+    // Attach drawer config to js settings
+    parent::prepareBuild($build, $vuid, $options);
+    // Attach visualn style libraries
+    $build['#attached']['library'][] = 'visualn_styles/slick';
+  }
+
+  /**
+   * @inheritdoc
+   */
+  public function defaultConfiguration() {
+    $default_config = [
+      'drawer_setup' => '', // slick config in json format
+      'data_keys' => '',
+    ];
+    return $default_config;
+  }
+
+  /**
+   * @inheritdoc
+   */
+  protected function getConfigurationForm(array $configuration = []) {
+    $configuration =  $configuration + $this->configuration + $this->defaultConfiguration();
+    $form = [];
+    // @todo: rename the key to drawer_setup to make it more generic
+    $form['drawer_setup'] = [
+      '#type' => 'textarea',
+      '#title' => t('Slick setup'),
+      '#default_value' => $configuration['drawer_setup'],
+    ];
+    // @todo: this will need ajax to update drawer fields subform without reopening
+    $form['data_keys'] = [
+      '#type' => 'textfield',
+      '#title' => t('Slick data keys'),
+      '#default_value' => $configuration['data_keys'],
+    ];
+    return $form;
+  }
+
+  /**
+   * @inheritdoc
+   */
+  public function jsId() {
+    return 'visualnSlickDrawer';
+  }
+
+  /**
+   * @inheritdoc
+   */
+  public function prepareJSConfig(array &$drawer_config) {
+    $drawer_config['slick_setup'] = json_decode($drawer_config['drawer_setup'], TRUE);
+  }
+
+  /**
+   * @inheritdoc
+   */
+  public function dataKeys() {
+    $slick_keys = trim($this->configuration['data_keys']);
+    if (!empty($slick_keys)) {
+      $data_keys = explode(',', $slick_keys);
+      // @todo: trim every key string
+    }
+    else {
+      $data_keys = [];
+    }
+
+    return $data_keys;
+  }
+
+}
